58cb4fd9f724898aec794c79d6c26233f8fc3df9
= <yeohchongyics@gmail.com>
2024-05-15 15:58:54 +0800

    Update documentation about previously unused rpcs in online-users.ts after updating nakama

--- online-users.ts
+++ online-users.ts
@@ -20,11 +20,6 @@ function rpcJoinOnlineUsersStream(ctx: nkruntime.Context, logger: nkruntime.Logg
     return JSON.stringify({ status: 'success' });
 }
 
-// This unused rpc function is supposed to return the list of online users
-// nk.streamUserList is supposed to be used to get the list of users in the online users stream
-// However, the function does not work properly for some strange reason. It only returns an empty array
-// even though the stream has users in it.
-// Instead, retrieving the users in the online users stream is done in the next-omegacity client side 
 function rpcGetOnlineUsers(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, payload: string): string {
     // Acts as hashmap that checks if a user is online
     const onlineUserIds: { [key: string]: boolean } = {};
@@ -38,10 +33,6 @@ function rpcGetOnlineUsers(ctx: nkruntime.Context, logger: nkruntime.Logger, nk:
     return JSON.stringify(onlineUserIds);
 }
 
-// Like rpcGetOnlineUsers, this function is also unused
-// rpcCountOnlineUsers is supposed to return the number of users in the online stream
-// However, it does not work properly. It always returns 0 even though the stream has users in it
-// as nk.streamUserList always returns an empty array
 function rpcCountOnlineUsers(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, payload: string): string {
     const presences = nk.streamUserList(onlineUsersStreamId);
     return presences.length.toString();
